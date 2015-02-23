541b9811cd81d1e55b6f11a6c2425b30a2df5174
bhiles <bennetthiles@gmail.com>
2015-02-23 18:07:07 -0500

    virtualenv config

--- system/env.zsh
+++ system/env.zsh
@@ -1,3 +1,7 @@
 export EDITOR='/Applications/Emacs.app/Contents/MacOS/Emacs'
 export GNUTERM='x11'
 export NODENV_VERSION=v0.10
+
+# Python's VirtualEnv
+export WORKON_HOME=~/Envs
+source /opt/boxen/homebrew/bin/virtualenvwrapper.sh
