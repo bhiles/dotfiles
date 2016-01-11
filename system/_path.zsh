80ae4ae7134ee52659914940029139e3ee83321d
bhiles <bennetthiles@gmail.com>
2016-01-11 17:27:30 -0500

    Update vars to the latest values

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,4 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/src/phab-tools/arcanist/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PYTHONSTARTUP=~/.pystartup
+export ANSIBLE_CONFIG=/etc/ansible/ansible.cfg
--- system/env.zsh
+++ system/env.zsh
@@ -1,7 +1,26 @@
 export EDITOR='/Applications/Emacs.app/Contents/MacOS/Emacs'
 export GNUTERM='x11'
-export NODENV_VERSION=v0.12.2
 
 # Python's VirtualEnv
 export WORKON_HOME=~/Envs
 source /opt/boxen/homebrew/bin/virtualenvwrapper.sh
+
+# Go
+export GOPATH=$HOME
+export GOBIN=$HOME
+
+# Boot2docker
+export BOOT2DOCKER_IP=$(boot2docker ip)
+
+# Java
+export JAVA_HOME=$(/usr/libexec/java_home)
+
+# Button Ansible Playbooks
+export ANSIBLE_CONFIG=/etc/ansible/ansible.cfg
+
+# Node
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+
+# Rbenv
+export RBENV_VERSION=2.2.3
